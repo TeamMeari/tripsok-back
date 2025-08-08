6201f0f08c8a646efb230325aa524d6c31391570
Chae <oumuamua121@gmail.com>
2025-08-08 15:50:02 +0900

    feat : 이메일 정규식 확인 및 중복 확인

--- back/tripsok_back/src/main/java/com/tripsok_back/dto/auth/request/EmailSignUpRequest.java
+++ back/tripsok_back/src/main/java/com/tripsok_back/dto/auth/request/EmailSignUpRequest.java
@@ -1,18 +1,23 @@
 package com.tripsok_back.dto.auth.request;
 
+import static com.tripsok_back.common.constants.RegexConstants.*;
+
 import java.util.List;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class EmailSignUpRequest {
-	@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+	@Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = NICKNAME_MESSAGE)
 	@NotNull(message = "닉네임은 필수 입력값입니다")
+	@Pattern(regexp = NICKNAME_REGEX, message = NICKNAME_MESSAGE)
 	private String nickname;
-	@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+	@Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = PASSWORD_MESSAGE)
 	@NotNull(message = "비밀번호는 필수 입력값입니다")
+	@Pattern(regexp = PASSWORD_REGEX, message = PASSWORD_MESSAGE)
 	private String password;
 	@Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "이메일 인증 후 사용자에게 발급되는 토큰입니다")
 	@NotNull(message = "이메일 인증 토큰은 필수 입력값입니다")
