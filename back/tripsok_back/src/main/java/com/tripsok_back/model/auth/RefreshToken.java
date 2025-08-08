b84f141de78e174743a06c957987f66ac1749141
Chae <oumuamua121@gmail.com>
2025-08-09 04:00:59 +0900

    refactor : 코드 정리

--- back/tripsok_back/src/main/java/com/tripsok_back/model/auth/RefreshToken.java
+++ back/tripsok_back/src/main/java/com/tripsok_back/model/auth/RefreshToken.java
@@ -4,8 +4,6 @@ import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
