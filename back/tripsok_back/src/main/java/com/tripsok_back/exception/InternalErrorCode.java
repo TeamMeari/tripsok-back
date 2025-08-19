80a18133166fe70e7e29789b9ab052130694d7fc
recreate.kang <recreate.kang@gmail.com>
2025-08-20 00:25:17 +0900

    feat: 내부 에러 enum 추가

--- back/tripsok_back/src/main/java/com/tripsok_back/exception/InternalErrorCode.java
+++ back/tripsok_back/src/main/java/com/tripsok_back/exception/InternalErrorCode.java
@@ -6,8 +6,9 @@ import lombok.Getter;
 public enum InternalErrorCode {
 	// Category 에러
 	CATEGORY_NOT_FOUND("해당 카테고리가 존재하지 않습니다"),
-	PLACE_DETAIL_NOT_FOUND("장소의 세부 정보가 TOUR API에 존재하지 않습니다");
-
+	PLACE_DETAIL_NOT_FOUND("장소의 세부 정보가 TOUR API에 존재하지 않습니다"),
+	JSON_PARSE_ERROR("JSON 변환 중 오류가 발생했습니다"),
+	INTERNAL_SERVER_ERROR("알수 없는 오류가 발생했습니다");
 	private final String errorMessage;
 
 	InternalErrorCode(String errorMessage) {
