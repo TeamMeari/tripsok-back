2b4fc12affeec9c9ec75f228af60315a5b7144ec
Chae <oumuamua121@gmail.com>
2025-08-30 21:58:08 +0900

    feat : Theme List 조회

---
+++ back/tripsok_back/src/main/java/com/tripsok_back/service/user/ThemeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.tripsok_back.service.user;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tripsok_back.dto.user.response.ThemeResponse;
+import com.tripsok_back.repository.ThemeRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ThemeServiceImpl implements ThemeService {
+	private ThemeRepository themeRepository;
+
+	@Override
+	public List<ThemeResponse> getThemeList() {
+		return themeRepository.findAll().stream().map(it-> new ThemeResponse(it.getId(), it.getType())).toList();
+	}
+}
