ea3e81449427d5a96999a2e9649ffe75ceaf63ca
recreate.kang <recreate.kang@gmail.com>
2025-09-01 13:43:57 +0900

    fix: restcontroller로 변경

--- back/tripsok_back/src/main/java/com/tripsok_back/controller/place/PlaceController.java
+++ back/tripsok_back/src/main/java/com/tripsok_back/controller/place/PlaceController.java
@@ -7,11 +7,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tripsok_back.dto.PageResponse;
 import com.tripsok_back.dto.place.PlaceBriefResponseDto;
@@ -33,7 +33,7 @@ import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/places")
 @RequiredArgsConstructor
 @Slf4j
--- back/tripsok_back/src/main/java/com/tripsok_back/dto/place/ReviewRequestDto.java
+++ back/tripsok_back/src/main/java/com/tripsok_back/dto/place/ReviewRequestDto.java
@@ -1,11 +1,11 @@
 package com.tripsok_back.dto.place;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public class ReviewRequestDto {
 
-	@NotNull(message = "PlaceId가 비었습니다")
+	@NotBlank(message = "PlaceId가 비었습니다")
 	Integer placeId;
-	@NotNull(message = "리뷰 내용을 입력해주세요")
+	@NotBlank(message = "리뷰 내용을 입력해주세요")
 	String review;
 }
