b04aa12ce2f9d0ae0685da9e28e0ce2b217afaa5
Chae <oumuamua121@gmail.com>
2025-09-06 22:49:28 +0900

    reafactor : 오타 수정

--- back/tripsok_back/src/main/java/com/tripsok_back/exception/AiException.java
+++ back/tripsok_back/src/main/java/com/tripsok_back/exception/AiException.java
@@ -6,6 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 public class AiException extends CustomInternalException {
 	public AiException(InternalErrorCode message, Throwable cause) {
 		super(message);
-		log.error("AiException occurred: {}, cause: {}", message, cause);
+		log.error("AiException occurred: {}, cause: {}", message, cause.getMessage());
 	}
 }
