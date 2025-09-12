fa882cf20c52de546202954caf4160d9317c2385
Chae <oumuamua121@gmail.com>
2025-09-13 00:04:08 +0900

    refactor : 쿼리 수정

--- back/tripsok_back/src/main/java/com/tripsok_back/repository/user/InterestPlaceRepository.java
+++ back/tripsok_back/src/main/java/com/tripsok_back/repository/user/InterestPlaceRepository.java
@@ -9,13 +9,11 @@ import org.springframework.stereotype.Repository;
 import com.tripsok_back.model.place.Place;
 import com.tripsok_back.model.user.InterestPlace;
 import com.tripsok_back.model.user.TripSokUser;
-import com.tripsok_back.type.PlaceJoinType;
 
 @Repository
 public interface InterestPlaceRepository extends JpaRepository<InterestPlace, Integer> {
 	InterestPlace findInterestPlaceByUserAndPlace(TripSokUser user, Place place);
 
-	@Query("SELECT ip FROM InterestPlace ip WHERE ip.user = :user AND (:lastId IS NULL OR ip.id < :lastId) AND (:type = com.tripsok_back.type.PlaceJoinType.ACCOMMODATION AND ip.place.accommodation IS NOT NULL) OR (:type = com.tripsok_back.type.PlaceJoinType.RESTAURANT AND ip.place.restaurant IS NOT NULL) OR (:type = com.tripsok_back.type.PlaceJoinType.TOUR AND ip.place.tour IS NOT NULL) ORDER BY ip.id desc")
-	Slice<InterestPlace> findInterestPlacesByUser(TripSokUser user, Pageable pageable, Integer lastId,
-		PlaceJoinType type);
+	@Query("SELECT ip FROM InterestPlace ip WHERE ip.user = :user AND (:lastId IS NULL OR ip.id < :lastId) AND ((:type = \"ACCOMMODATION\" AND ip.place.accommodation IS NOT NULL) OR (:type = \"RESTAURANT\" AND ip.place.restaurant IS NOT NULL) OR (:type = \"TOUR\" AND ip.place.tour IS NOT NULL)) ORDER BY ip.id DESC")
+	Slice<InterestPlace> findInterestPlacesByUser(TripSokUser user, Pageable pageable, Integer lastId, String type);
 }
