95520f9d654d1b9817f7a7c849a94113ea60eaa9
Chae <oumuamua121@gmail.com>
2025-10-16 22:44:34 +0900

    feat : 비밀번호 재설정시 닉네임 반환 및 비밀번호 정규식 추가

--- back/tripsok_back/src/main/java/com/tripsok_back/dto/auth/request/ResetPasswordRequest.java
+++ back/tripsok_back/src/main/java/com/tripsok_back/dto/auth/request/ResetPasswordRequest.java
@@ -1,13 +1,17 @@
 package com.tripsok_back.dto.auth.request;
 
+import static com.tripsok_back.common.constants.RegexConstants.*;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class ResetPasswordRequest {
 	@Schema(requiredMode = Schema.RequiredMode.REQUIRED)
 	@NotNull(message = "비밀번호는 필수 입력값입니다")
+	@Pattern(regexp = PASSWORD_REGEX, message = PASSWORD_MESSAGE)
 	private String password;
 	@Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "이메일 인증 후 사용자에게 발급되는 토큰입니다")
 	@NotNull(message = "이메일 인증 토큰은 필수 입력값입니다")
