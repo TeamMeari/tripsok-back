d9a3b78a111e2c2e538a3e0e5324f605116c0a3a
Chae <oumuamua121@gmail.com>
2025-10-31 01:05:52 +0900

    refactor : JWT 예외발생 시 errorMessage 알맞게 응답하도록 수정

--- back/tripsok_back/src/main/java/com/tripsok_back/security/filter/JwtCheckFilter.java
+++ back/tripsok_back/src/main/java/com/tripsok_back/security/filter/JwtCheckFilter.java
@@ -7,8 +7,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tripsok_back.exception.AuthException;
 import com.tripsok_back.exception.ErrorCode;
+import com.tripsok_back.exception.JwtException;
+import com.tripsok_back.exception.handler.ErrorResponse;
 import com.tripsok_back.repository.auth.RedisBlackListAccessTokenRepository;
 import com.tripsok_back.security.jwt.JwtUtil;
 
@@ -25,6 +28,7 @@ import lombok.extern.log4j.Log4j2;
 public class JwtCheckFilter extends OncePerRequestFilter {
 	private final JwtUtil jwtUtil;
 	private final RedisBlackListAccessTokenRepository blackListAccessTokenRepository;
+	private final ObjectMapper objectMapper;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
@@ -35,12 +39,25 @@ public class JwtCheckFilter extends OncePerRequestFilter {
 			return;
 		}
 		String token = authHeader.substring(7);
+		try {
 		if (blackListAccessTokenRepository.existsByToken(token)) {
 			throw new AuthException(ErrorCode.INVALID_TOKEN, "해당 토큰은 블랙리스트에 있습니다.");
 		}
-		Integer userId = jwtUtil.validateAndExtract(token, "userId", Integer.class);
-		SecurityContextHolder.getContext()
-			.setAuthentication(new UsernamePasswordAuthenticationToken(userId, token, jwtUtil.getAuthorities(token)));
-		filterChain.doFilter(request, response);
+			Integer userId = jwtUtil.validateAndExtract(token, "userId", Integer.class);
+			SecurityContextHolder.getContext()
+				.setAuthentication(new UsernamePasswordAuthenticationToken(userId, token, jwtUtil.getAuthorities(token)));
+			filterChain.doFilter(request, response);
+		}catch (JwtException | AuthException e){
+			SecurityContextHolder.clearContext();
+			if (response.isCommitted()) {
+				return;
+			}
+			response.reset();
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.setContentType("application/json;charset=UTF-8");
+			ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getCode(),
+				e.getMessage());
+			objectMapper.writeValue(response.getOutputStream(), errorResponse);
+		}
 	}
 }
