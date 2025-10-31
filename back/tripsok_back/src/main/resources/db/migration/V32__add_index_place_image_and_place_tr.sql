5a565faadacb9a3a6c8c2e12b60fa2ac7fdea2ef
Chae <oumuamua121@gmail.com>
2025-10-31 23:47:42 +0900

    refactor : 리뷰적용

--- back/tripsok_back/src/main/resources/db/migration/V32__add_index_place_image_and_place_tr.sql
+++ back/tripsok_back/src/main/resources/db/migration/V32__add_index_place_image_and_place_tr.sql
@@ -1,4 +1,4 @@
-CREATE INDEX idx_accommodation_image_place ON ACCOMMODATION_IMAGE(ACCOMMODATION_ID);
-CREATE INDEX idx_place_tr_place ON PLACE_TR(PLACE_ID);
-CREATE INDEX idx_restaurant_image_place ON RESTAURANT_IMAGE(RESTAURANT_ID);
-CREATE INDEX idx_tour_image_place ON TOUR_IMAGE(TOUR_ID);
+CREATE INDEX idx_accommodation_image_accommodation_id ON ACCOMMODATION_IMAGE(ACCOMMODATION_ID);
+CREATE INDEX idx_place_tr_place_id ON PLACE_TR(PLACE_ID);
+CREATE INDEX idx_restaurant_image_restaurant_id ON RESTAURANT_IMAGE(RESTAURANT_ID);
+CREATE INDEX idx_tour_image_tour_id ON TOUR_IMAGE(TOUR_ID);
