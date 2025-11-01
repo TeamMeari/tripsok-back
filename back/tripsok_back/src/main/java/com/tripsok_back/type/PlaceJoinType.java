3fa864cc47731c5a1bebaf7d6d2e26ec755a3bcc
Recreate-Kang <recreate.kang@gmail.com>
2025-11-01 13:20:30 +0900

    feat: 장소 타입 관련 로직 추가

--- back/tripsok_back/src/main/java/com/tripsok_back/type/PlaceJoinType.java
+++ back/tripsok_back/src/main/java/com/tripsok_back/type/PlaceJoinType.java
@@ -1,5 +1,6 @@
 package com.tripsok_back.type;
 
+import com.tripsok_back.dto.place.PlaceDocument;
 import com.tripsok_back.exception.ErrorCode;
 import com.tripsok_back.exception.PlaceException;
 import com.tripsok_back.model.place.Place;
@@ -13,6 +14,18 @@ public enum PlaceJoinType {
 		} else if (place.getRestaurant() != null) {
 			return PlaceJoinType.RESTAURANT;
 		} else if (place.getTour() != null) {
+			return PlaceJoinType.TOUR;
+		} else {
+			throw new PlaceException(ErrorCode.INVALID_TOUR_TYPE);
+		}
+	}
+
+	public static PlaceJoinType getPlaceType(PlaceDocument place) {
+		if (place.getType() != null) {
+			return PlaceJoinType.ACCOMMODATION;
+		} else if (place.getType() != null) {
+			return PlaceJoinType.RESTAURANT;
+		} else if (place.getType() != null) {
 			return PlaceJoinType.TOUR;
 		} else {
 			throw new PlaceException(ErrorCode.INVALID_TOUR_TYPE);
