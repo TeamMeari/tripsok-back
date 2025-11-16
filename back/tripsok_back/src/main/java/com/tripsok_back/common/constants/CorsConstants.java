5a5427b95ca987a8265f1e3264a9061a7764c9a6
Chae <oumuamua121@gmail.com>
2025-11-16 21:02:54 +0900

    refactor : jwtfilter cors 처리

---
+++ back/tripsok_back/src/main/java/com/tripsok_back/common/constants/CorsConstants.java
@@ -0,0 +1,30 @@
+package com.tripsok_back.common.constants;
+
+import java.util.List;
+
+public final class CorsConstants {
+	public static final List<String> ALLOWED_ORIGINS = List.of(
+		"http://localhost:8080",
+		"http://localhost:5173",
+		"http://localhost:3000",
+		"http://localhost:4173",
+		"https://trip-sok.jayden-bin.cc",
+		"https://www.tourang.site",
+		"https://tripsok-front.vercel.app"
+	);
+	public static final List<String> ALLOWED_HEADERS = List.of(
+		"Authorization", "Content-Type"
+	);
+
+	public static final String ALLOWED_HEADERS_CSV = String.join(",", ALLOWED_HEADERS);
+
+	private CorsConstants() {
+	}
+
+	public static boolean isAllowedOrigin(String origin){
+		for (String allowedOrigin : ALLOWED_ORIGINS) {
+			if(origin.equals(allowedOrigin)) return true;
+		}
+		return false;
+	}
+}
