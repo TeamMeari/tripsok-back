39fc5a0ee74b001fb64b035c90ebde2be585d1f7
Chae <oumuamua121@gmail.com>
2025-11-18 10:28:08 +0900

    refactor : lat 에러 안나도록 수정

--- back/tripsok_back/src/main/java/com/tripsok_back/dto/booking/response/GetBookingSpotSetResponse.java
+++ back/tripsok_back/src/main/java/com/tripsok_back/dto/booking/response/GetBookingSpotSetResponse.java
@@ -1,10 +1,9 @@
 package com.tripsok_back.dto.booking.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.tripsok_back.model.booking.BookingSpot;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-
-import com.tripsok_back.model.booking.BookingSpot;
 
 public record GetBookingSpotSetResponse(
 	Integer placeId,
@@ -20,9 +19,8 @@ public record GetBookingSpotSetResponse(
     @Schema(description = "기존 latitude 필드입니다. `lat` 필드를 사용해주세요. (deprecated)", deprecated = true, example = "37.5665")
 	@JsonProperty("latitude")
 	@Deprecated
-	public Double getLatitudeDeprecated() { // Changed method name and added @Deprecated annotation
-		//return lat.doubleValue();
-		throw new UnsupportedOperationException("This field is deprecated. Use 'lat' instead.");
+	public Double getLatitudeDeprecated() {
+		return lat;
 	}
 
     @Schema(description = "기존 longitude 필드입니다. `lng` 필드를 사용해주세요. (deprecated)", deprecated = true, example = "126.9780")
